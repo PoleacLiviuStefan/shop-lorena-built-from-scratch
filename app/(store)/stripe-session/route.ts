de44443e98caa90aefb2620a5302606643233599
Poleac Liviu Stefan <liviu-stefan.poleac@s.unibuc.ro>
2025-02-01 01:20:40 +0200

    change env var name

--- app/(store)/stripe-session/route.ts
+++ app/(store)/stripe-session/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.NEXT_STRIPE_SECRET_KEY as string, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: '2024-12-18.acacia',
 });
 
